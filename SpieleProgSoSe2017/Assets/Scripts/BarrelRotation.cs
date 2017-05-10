8b6818d17eb8f392f6308bb00e744b186e4c53f5
philipp-bous <wolf1bous@googlemail.com>
2017-05-10 22:42:54 +0200

    Vorbereitung fuer Prototyp-Treffen
    Noch nicht getestet. wir werden die skripts anpassen muessen auf eine schoene Ausrichtung des KS. Die soltlen einheiltich sein in allen Levels, sonst kriegen wir mit den scripts unnötig echt fiese Probleme.

---
+++ SpieleProgSoSe2017/Assets/Scripts/BarrelRotation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretRotation2 : MonoBehaviour
+{
+    public int barrelLiftSpeed;
+    public int minAnglePositive;
+    public int maxAnglePosositive;
+    Quaternion originalRot;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //das hier ist jetzt die verbesserte Variante, bei der das relative Koordinatensystem der muzzle genauso bleibt wies soll und zwei achsen jeweils gelocked sind
+        //was ich noch nicht verstehe ist, warum diese multiplikation mit dem originalwert nötig ist ausserdem spinnt der bone noch bisschen rum mit den rotationswerten
+        originalRot = transform.rotation;
+        if (Input.GetKey(KeyCode.A))
+        {
+            //hier muss man bissl aufpassen, weil der einem die winkel nur positiv gibt, daher nicht 0 wäre noch zu überlegen, was man macht, wenn man fahrzeuge hat, die nach unten zielen können
+            if (Vector3.Angle(transform.up, Vector3.right) > minAnglePositive)
+            {
+                transform.rotation = originalRot * Quaternion.AngleAxis(barrelLiftSpeed * Time.deltaTime, Vector3.left);
+            }
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (Vector3.Angle(transform.up, Vector3.right) < maxAnglePosositive) {
+                transform.rotation = originalRot * Quaternion.AngleAxis(-barrelLiftSpeed * Time.deltaTime, Vector3.left);
+            }
+        }
+
+    }
+}
---
+++ SpieleProgSoSe2017/Assets/Scripts/ShellRotation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShellRotation : MonoBehaviour
+{
+    Vector3 vel;
+    Vector3 newRot;
+    bool collisionDetected = false;
+
+    private void Start()
+    {
+        newRot = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!collisionDetected)
+        {
+            vel = GetComponent<Rigidbody>().velocity;
+            float angle = Mathf.Atan2(vel.y, vel.x) * Mathf.Rad2Deg + 180;
+            newRot.z = angle;
+            transform.eulerAngles = newRot;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        collisionDetected = true;
+    }
+}
+
