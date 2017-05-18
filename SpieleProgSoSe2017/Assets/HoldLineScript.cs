82fe39f19276f86febe33d7c937b60f208df08e0
Florian kruschewski <firuniantwo@web.de>
2017-05-18 09:36:43 +0200

    Movement Fix

---
+++ SpieleProgSoSe2017/Assets/HoldLineScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldLineScript : MonoBehaviour {
+    float holdVec;
+    float holdPos;
+    Vector3 rotVector;
+    Vector3 posVector;
+    // Use this for initialization
+    void Start () {
+        rotVector = transform.rotation.eulerAngles;
+        holdVec = rotVector.y;
+        posVector = transform.position;
+        holdPos = posVector.z;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rotVector = transform.rotation.eulerAngles;
+        rotVector.y = holdVec;
+        transform.rotation = Quaternion.Euler(rotVector);
+        posVector = transform.position;
+        posVector.z = holdPos;
+        transform.position = posVector;
+    }
+}
