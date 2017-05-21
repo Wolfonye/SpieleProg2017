5a36bc2fe95e2d2c279494e28e16340b194d91b5
philipp-bous <wolf1bous@googlemail.com>
2017-05-21 20:08:28 +0200

    Tank Tests
    Ja...mein neuer Ansatz ist bei simplen Dingen wirklich schön, aber leider mit sehr blöden Krankheiten versehen, die zu fixen vermutlich zu aufwändig wird.

---
+++ SpieleProgSoSe2017/Assets/Scripts/CarMovementAlternative.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovementAlternative : MonoBehaviour
+{
+
+    public GameObject rigidBody;
+    public float speed;
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rigidBody.transform.position = rigidBody.transform.position + movement * Time.deltaTime * speed;
+    }
+}
