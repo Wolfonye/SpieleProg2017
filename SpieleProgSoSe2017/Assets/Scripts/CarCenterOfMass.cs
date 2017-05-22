c05e683fe31e408a7d0da48c5693511890e4ca42
philipp-bous <wolf1bous@googlemail.com>
2017-05-23 00:35:54 +0200

    Car tests (mal wieder)
    es wird besser! unfassbar

---
+++ SpieleProgSoSe2017/Assets/Scripts/CarCenterOfMass.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarCenterOfMass : MonoBehaviour {
+	public Vector3 com ;
+	public Rigidbody rb;
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+		com = new Vector3 (0.3f,0.3f,0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.centerOfMass = com;
+		Debug.DrawRay(com, com + new Vector3 (0.5f, 0f,0.0f),Color.green);
+	}
+}
