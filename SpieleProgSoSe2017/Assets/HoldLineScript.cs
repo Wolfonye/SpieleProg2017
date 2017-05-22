91223b9d85f9fff15e3d1fe0522ba20f089666c3
Florian kruschewski <firuniantwo@web.de>
2017-05-22 10:21:02 +0200

    Auto stash before merge of "vehicle_movement" and "origin/vehicle_movement"

--- SpieleProgSoSe2017/Assets/HoldLineScript.cs
+++ SpieleProgSoSe2017/Assets/HoldLineScript.cs
@@ -7,13 +7,17 @@ public class HoldLineScript : MonoBehaviour {
     float holdPos;
     Vector3 rotVector;
     Vector3 posVector;
+    public static bool rightFree;
+    public static bool leftFree;
     // Use this for initialization
     void Start () {
         rotVector = transform.rotation.eulerAngles;
         holdVec = rotVector.y;
         posVector = transform.position;
         holdPos = posVector.z;
-    }
+        rightFree = true;
+        leftFree = true;
+}
 	
 	// Update is called once per frame
 	void Update () {
@@ -23,5 +27,28 @@ public class HoldLineScript : MonoBehaviour {
         posVector = transform.position;
         posVector.z = holdPos;
         transform.position = posVector;
+
+        if (Input.GetKeyDown("a"))
+        {
+            if (leftFree == true)
+            {
+                holdPos = holdPos + 5;
+            }
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            if (rightFree == true)
+            {
+                holdPos = holdPos - 5;
+            }           
+        }
     }
 }
+
+/*[System.Serializable]
+public class CollisionInfo
+{
+    public Collider left;
+    public Collider right;
+    
+}*/
---
+++ SpieleProgSoSe2017/Assets/RightCollTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightCollTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        HoldLineScript.rightFree = false;
+        Debug.Log("Coll");
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        HoldLineScript.rightFree = false;
+        Debug.Log("Coll");
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        HoldLineScript.rightFree = true;
+        Debug.Log("CollEx");
+    }
+}
