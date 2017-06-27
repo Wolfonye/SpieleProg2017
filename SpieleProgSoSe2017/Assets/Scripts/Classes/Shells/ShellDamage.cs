258b6c9c51f59e1be7d0098af53d3f7c6dc815e2
philipp-bous <wolf1bous@googlemail.com>
2017-06-27 19:07:50 +0200

    RoundFinished Menu implemented

--- SpieleProgSoSe2017/Assets/Scripts/Classes/Shells/ShellDamage.cs
+++ SpieleProgSoSe2017/Assets/Scripts/Classes/Shells/ShellDamage.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShellDamage : MonoBehaviour {
-	public int Damage = 100;
+	public int Damage = 250;
 	// Use this for initialization
 	void Start () {
 		
