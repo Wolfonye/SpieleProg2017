ce1fe0fde64509b83f0e53057792fa6799b18a34
philipp-bous <wolf1bous@googlemail.com>
2017-08-08 15:14:18 +0200

    Safety First

--- SpieleProgSoSe2017/Assets/Scripts/Classes/GameControl/ActiveObjects.cs
+++ SpieleProgSoSe2017/Assets/Scripts/Classes/GameControl/ActiveObjects.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-//soll eine Containerklasse für verschiedene Objekte sein, deren Aktivität besondere Bedeutung haben kann
+//soll eine Containerklasse für verschiedene Objekte sein, deren Aktivität/Wert besondere Bedeutung haben kann
 //Ursprung ist die Einführung dreier Cam-Modi; zwei waren noch relativ leicht anders zu handhaben, aber für
 //drei Modi war das alte Design nicht gut und ich lagere das jetzt aus, da diese Information ja evtl noch woanders wichtig sein könnte
 //(hätte ja auch alles im Cam-Script bleiben können)
