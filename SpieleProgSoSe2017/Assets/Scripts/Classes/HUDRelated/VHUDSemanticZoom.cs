4cdbca4e825149f74e8d2d7cde2f6ab522f974e3
philipp-bous <wolf1bous@googlemail.com>
2017-08-18 10:10:02 +0200

    Semantic Zoom update

--- SpieleProgSoSe2017/Assets/Scripts/Classes/HUDRelated/VHUDSemanticZoom.cs
+++ SpieleProgSoSe2017/Assets/Scripts/Classes/HUDRelated/VHUDSemanticZoom.cs
@@ -8,47 +8,55 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-//Soll semantischen Zoom bzgl der Vehicle HUDs realisieren. ZB soll erst ab einer gewissen Nähe die HP in Zahlen angezeigt werden
+//Soll semantischen Zoom bzgl der Vehicle HUDs realisieren.
+/**
+ *Idee: lineares Fade in/Fade out mittels Veränderung der Alphawerte; von außen kann angegeben werden, ab welcher Distanz das Fade in beginnen und ab
+ *wann der maximale Wert erreicht werden soll.
+ */
 public class VHUDSemanticZoom : MonoBehaviour {
-	//welche Distanz unterschritten werden muss, damit HP und AP als Text angezeigt werden
-	public int drawDistanceForBarTexts;
-
+	//welche Distanz unterschritten werden muss, damit HP und AP als Text angezeigt werden.Ab welcher Distanz soll der maximale Alphawert erreicht sein?
+	public float drawDistanceForBarTexts;
+	public float maxAlphaPoint;
 
 	public Text healthText;
 	public Text actionPointText;
 
 	//ref auf die MainCam...zu ihr wollen wir die Distanz berechnen;
 	private Camera mainCam;
-	//wie weit ist die Cam vom fraglichen HUD entfernt?
-	private float currentCamDistance; 
+	//wie weit ist die Cam vom fraglichen HUD entfernt? 
+	private float currentCamDistance;
+
 	//soll die ActionBar überhaupt gezeigt werden?
 	private bool drawActionbar;
 
+	private Color helperColor;
 	// Use this for initialization
 	void Start () {
-		healthText.gameObject.SetActive (false);
-		actionPointText.gameObject.SetActive (false);
 		mainCam = GameObject.FindWithTag ("MainCamera").GetComponent<Camera>();
-		if (!(ActiveObjects.getActiveGameModeID () == "GAS_MODE")) {
-			drawActionbar = false;
-		} else {
+		if (ActiveObjects.getActiveGameModeID () == "GAS_MODE") {
 			drawActionbar = true;
+			healthText.gameObject.SetActive(true);
+			actionPointText.gameObject.SetActive (true);
+		} else {
+			drawActionbar = false;
+			healthText.gameObject.SetActive (false);
+			actionPointText.gameObject.SetActive (false);
 		}
+		helperColor = healthText.color;
 	}
-		
+
 	// Update is called once per frame
 	void Update () {
 		currentCamDistance = Vector3.Distance (mainCam.transform.position, gameObject.transform.position);
-		if (drawActionbar && (currentCamDistance < drawDistanceForBarTexts)) {
-			actionPointText.gameObject.SetActive (true);
-		} else {
-			actionPointText.gameObject.SetActive (false);
+		Debug.Log("currentcamdistance: " + currentCamDistance);
+		if(currentCamDistance < drawDistanceForBarTexts && currentCamDistance > maxAlphaPoint){
+			helperColor.a = -currentCamDistance/(drawDistanceForBarTexts - maxAlphaPoint)+1; //Lineare Veränderung des ALphawertes
+			Debug.Log("helpercolor: " + helperColor);
 		}
-
-		if (currentCamDistance < drawDistanceForBarTexts) {
-			healthText.gameObject.SetActive (true);
-		} else {
-			healthText.gameObject.SetActive (false);
+		if (currentCamDistance < maxAlphaPoint){
+			helperColor.a = 1;
 		}
+		healthText.color = helperColor;
+		actionPointText.color = helperColor;
 	}
 }
