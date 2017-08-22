5ef41e5522fac8ab5bf7f54e0192d14d7af326e4
philipp-bous <wolf1bous@googlemail.com>
2017-08-22 20:26:14 +0200

    RecognieKeyInput added

---
+++ SpieleProgSoSe2017/Assets/Scripts/Classes/Menus/RecognizeKeyInput.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class RecognizeKeyInput : MonoBehaviour {
+	private int[] values;
+	private KeyCode[] keyCodes;
+	void Awake() {
+   	 	values = (int[])System.Enum.GetValues(typeof(KeyCode));
+		keyCodes = new KeyCode[values.Length];
+		for(int i = 0; i < values.Length; i++){
+			keyCodes[i] = (KeyCode)values[i];
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void keyWait(){
+		StartCoroutine(waitForInput());
+	}
+
+	private IEnumerator waitForInput(){
+		bool inputReceived = false;
+		while(!inputReceived){
+			for(int i = 0; i < values.Length; i++) {
+				if(Input.GetKey(keyCodes[i])){
+					//Debug.Log("Gedrueckte Taste: " + keyCodes[i].ToString());
+					inputReceived = true;
+				}
+			}
+			yield return null;
+		}
+	}
+}
+
+
