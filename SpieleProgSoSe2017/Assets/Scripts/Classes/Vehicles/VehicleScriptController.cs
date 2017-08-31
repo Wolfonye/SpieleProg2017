0771811ed89a436a4fd17c3f66a8bb76e80aedf6
philipp-bous <wolf1bous@googlemail.com>
2017-08-31 18:56:21 +0200

    Setup für Cycling-Rework

---
+++ SpieleProgSoSe2017/Assets/Scripts/Classes/Vehicles/VehicleScriptController.cs
@@ -0,0 +1,16 @@
+﻿/*
+ *Author: Philipp Bous
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Dient dazu den Cycler nicht zu überladen; dieses Skript soll bei Aktivierung alle relevanten Skripte eines Vehicles einschalten und bei Deaktivierung
+//alle entsprechenden Skripte wieder abschalten; das heit Steuerbarkeit, Wirken von Effekten usw.
+//dadurch muss der Cycler nur noch dieses eine Skript aktivieren, welches dann die eigentliche Arbeit macht.
+public class VehicleScriptController : MonoBehaviour {
+
+
+}
