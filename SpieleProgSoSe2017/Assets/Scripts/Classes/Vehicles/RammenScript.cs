d913e942575eba2ca17cd7198a5d5d470363006d
philipp-bous <wolf1bous@googlemail.com>
2017-09-15 11:50:15 +0200

    Rammen fix

--- SpieleProgSoSe2017/Assets/Scripts/Classes/Vehicles/RammenScript.cs
+++ SpieleProgSoSe2017/Assets/Scripts/Classes/Vehicles/RammenScript.cs
@@ -4,24 +4,25 @@ using UnityEngine;
 
 public class RammenScript : MonoBehaviour {
 	CarMovement Wheels;
-	float TimeEllapsed = 0;
 	// Use this for initialization
 	void Start () {
 		Wheels = gameObject.GetComponentInChildren<CarMovement> ();
 	}
-		
+	
+	// Update is called once per frame
 	void Update () {
-		TimeEllapsed += Time.deltaTime;
+		
 	}
-
 
 	private void OnCollisionEnter(Collision collision)
 	{
 		//Debug.Log ("test");
-		if (collision.gameObject.tag == "Tank" && TimeEllapsed > 1){
-			TimeEllapsed = 0;
+		if (collision.gameObject.tag == "Tank"){
 			//Debug.Log ("Gerammt");
 			int dmg = (int) Wheels.speed;
+			if(dmg > 80){
+				dmg = 80;
+			}
 			//Debug.Log (dmg);
 			collision.transform.GetComponent<TakeDamage> ().TakeExplosion(dmg);
 			gameObject.GetComponent<TakeDamage> ().TakeExplosion(dmg);
